9f8d01e2db36cbb50081860c18a59ea1e1e4a698
chaos <497386856@qq.com>
2017-08-30 19:56:35 +0800

    1007 有一例错误

---
+++ a/1007.py
@@ -0,0 +1,23 @@
+if (__name__ == "__main__"):
+    count = raw_input()
+    num = [int(x) for x in raw_input().split(' ')]
+    tempsum = 0
+    f=0
+    l=0
+    maxf = maxl = maxsum = num[0]
+    for i in num:
+        if (tempsum == 0):
+            f = i
+        if (i >= 0):
+            tempsum += i
+            if (tempsum > maxsum):
+                l = i
+                maxsum = tempsum
+                maxf = f
+                maxl = l
+            continue
+        tempsum += i
+        if (tempsum < 0):
+            tempsum = 0
+
+    print maxsum, maxf, maxl
