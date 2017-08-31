5ed399f191af6a61fc9c45f4c7623c8626530773
chaos <497386856@qq.com>
2017-08-31 19:11:23 +0800

    finish 1012

--- a/1012.py
+++ a/1012.py
@@ -6,34 +6,33 @@ def findRank(grade,ID):
 
 if(__name__=="__main__"):
     record_num,query_num  = [int(x) for x in raw_input().split(' ')]
-    A = []
-    C = []
-    M = []
-    E = []
+    records = []
+    Score = [[0 for i in range(101)], [0 for i in range(101)], [0 for i in range(101)], [0 for i in range(101)]]
+
+    res = []
+
     for i in range(record_num):
         l = raw_input().split(' ')
         ID = l[0]
         c,m,e =[int(x) for x in l[1:]]
         a = int((c+m+e)/3)
-        A.append([ID,a])
-        C.append([ID, c])
-        M.append([ID, m])
-        E.append([ID, e])
-
-    for i in [A,C,M,E]:
-        i.sort(key=lambda x:x[1],reverse=True)
-
-    res = []
-    s = ['A','C','M','E']
+        records.append([ID,a,c,m,e])
+        for col,grade in enumerate([a,c,m,e]):
+            for i in range(grade):
+                Score[col][i]+=1
+    s = ["A","C","M","E"]
+    names = [x[0] for x in records]
     for i in range(query_num):
         ID = raw_input()
-        temp = [findRank(grade=g,ID=ID) for g in [A,C,M,E]]
-        if(-1 in temp):
+        if(ID not in names):
             res.append('N/A')
         else:
-            bestIndex,bestRank = min(enumerate(temp),key=lambda x:x[1])
-            # if(temp.count(bestRank)>1):
-            #     bestIndex = temp.index(bestRank)
-            res.append('%d %s' % (bestRank+1,s[bestIndex]))
+            score = [Score[i][x] for i,x in enumerate(records[names.index(ID)][1:])]
+            bestGrade = min(score)
+            bestIndex = score.index(bestGrade)
+            res.append('%d %s' % (bestGrade+1,s[bestIndex]))
 
     print '\n'.join(res)
+
+
+
